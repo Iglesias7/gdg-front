f2ba0c0d21122c7bac4bad163f9c96c96c3f4cde
Chendjou Iglesias <ichendjou.toum@gmail.com>
2022-11-17 20:08:45 +0100

    Add elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { CoreModule } from './core/core.module';
 // import { WidgetModule } from './core/widgets/widget.module';
